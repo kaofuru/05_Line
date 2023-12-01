6e6e94dca4561e88dea5832f9c57b4201613009d
kaofuru <hnks.ff@gmail.com>
2023-12-01 22:50:22 +0900

    add

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Line from './components/Line';
+import SignIn from './components/SignIn';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase.js";
+  
+  
+function App() {
+  const [user] = useAuthState(auth);
+  return(
+    <div>
+      {user ? <Line /> :<SignIn />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignOut.js
@@ -0,0 +1,16 @@
+import { Button } from '@mui/material'
+import {auth} from "../firebase.js"
+import React from 'react'
+
+function SignOut() {
+  return (
+    <div>
+        <Button onClick={() => auth.signOut()}
+        >
+            サインアウト
+        </Button>
+    </div>
+  )
+}
+
+export default SignOut;
